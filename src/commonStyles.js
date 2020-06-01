4a29f01d09b4ace527ac044d1046a8a7cef32fcc
Marcos Fraga <marcosjsfraga@gmail.com>
2020-05-31 22:25:58 -0300

    Changes

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -4,6 +4,9 @@ export default {
 	fontFamily: 'Product Sans',
 	colors: {
 		today: '#b13b44',
+		tomorrow: '#c9742e',
+		week: '#15721e',
+		month: '#1631be',
 		secondary: '#fff',
 		mainText: '#222',
 		subText: "#555"
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -15,10 +15,13 @@ import 'moment/locale/pt-br';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from "react-native-vector-icons/FontAwesome";
 import commonStyles from '../commonStyles';
-import todayImage from '../../assets/imgs/today.jpg';
 import Task from '../components/Task';
 import AddTask from "./AddTask";
 import { server, showError, showSuccess } from '../common';
+import todayImage from '../../assets/imgs/today.jpg';
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg';
+import weekImage from '../../assets/imgs/week.jpg';
+import monthImage from '../../assets/imgs/month.jpg';
 
 const initialState = { 
 	showDoneTasks: true,
@@ -46,7 +49,7 @@ export default class TaskList extends Component {
 
 	loadTasks = async () => {
         try {
-			const maxDate = moment().format('YYYY-MM-DD 23:59:59');
+			const maxDate = moment().add({ days: this.props.daysAhead }).format('YYYY-MM-DD 23:59:59');
 			const res = await axios.get(`${server}/tasks?date=${maxDate}`);
 			this.setState({ tasks: res.data }, this.filterTasks);
         } catch (error) {
@@ -110,7 +113,25 @@ export default class TaskList extends Component {
 		} catch (error) {
 			showError(error);
 		}
-	} 
+	}
+
+	getImage = () => {
+		switch(this.props.daysAhead) {
+			case 0: return todayImage
+			case 1: return tomorrowImage
+			case 7: return weekImage
+			case 30: return monthImage
+		}
+	}
+
+	getColor = () => {
+		switch(this.props.daysAhead) {
+			case 0: return commonStyles.colors.today
+			case 1: return commonStyles.colors.tomorrow
+			case 7: return commonStyles.colors.week
+			case 30: return commonStyles.colors.month
+		}
+	}
 
 	render() {
 		const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
@@ -124,8 +145,15 @@ export default class TaskList extends Component {
 					onSave={ this.addTask }
 				/>
 				
-				<ImageBackground source={todayImage} style={styles.background}>
+				<ImageBackground source={this.getImage()} style={styles.background}>
 					<View style={styles.iconBar}>
+						<TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+							<Icon 
+								name='bars'
+								size={25}
+								color={commonStyles.colors.secondary}
+							/>
+						</TouchableOpacity>
 						<TouchableOpacity onPress={this.toggleFilter}>
 							<Icon 
 								name={ this.state.showDoneTasks ? "eye" : "eye-slash" }
@@ -135,7 +163,7 @@ export default class TaskList extends Component {
 						</TouchableOpacity>
 					</View>
 					<View style={styles.titleBar}>
-						<Text style={styles.title}>Hoje</Text>
+						<Text style={styles.title}>{this.props.title}</Text>
 						<Text style={styles.subtitle}>{today}</Text>
 					</View>
 				</ImageBackground>
@@ -151,7 +179,7 @@ export default class TaskList extends Component {
 					/>
 				</View>
 				<TouchableOpacity 
-					style={styles.addButton}
+					style={[styles.addButton, { backgroundColor: this.getColor() }]}
 					activeOpacity={0.7}
 					onPress={() => this.setState({ showAddTask: true })} >
 					<Icon name='plus'size={20} color={commonStyles.colors.secondary}/>
@@ -192,7 +220,7 @@ const styles = StyleSheet.create({
 	iconBar: {
 		flexDirection: "row",
 		marginHorizontal: 20,
-		justifyContent: "flex-end",
+		justifyContent: "space-between",
 		marginTop: Platform.OS === 'ios' ? 40 : 10
 	},
 	addButton: {
@@ -202,10 +230,7 @@ const styles = StyleSheet.create({
 		width: 50,
 		height: 50,
 		borderRadius: 25,
-		backgroundColor: commonStyles.colors.today,
 		justifyContent: 'center',
 		alignItems: 'center',
-
 	}
-
 });
